bff16ce0bc3980236202679d73423863f779cdde
Jun Wako <wakojun@gmail.com>
2015-02-19 17:52:28 +0900

    Tweak for my oven and Tamura leaded soder paste

--- src/HeadlessReflowOven.ino
+++ src/HeadlessReflowOven.ino
@@ -1,53 +1,14 @@
 /*********************************************************************
-ReflowOvenFLily - Written to work with ReflowControllerC
-LilyPadUSB.bootloader.extended_fuses=0xce
-LilyPadUSB.bootloader.high_fuses=0xd8
-LilyPadUSB.bootloader.low_fuses=0xff
+  Headless ReflowController - just with button, buzzer and LED
 
-           Magic Happens Here!
-* 225-|                                               x  x  
-*     |                                            x        x
-*     |                                         x              x
-*     |                                      x                    x
-* 200-|                                   x                          x
-*     |                              x    |                          |   x   
-*     |                         x         |                          |       x
-*     |                    x              |                          |
-* 150-|               x                   |                          |
-*     |             x |                   |                          |
-*     |           x   |                   |                          | 
-*     |         x     |                   |                          | 
-*     |       x       |                   |                          | 
-*     |     x         |                   |                          |
-*     |   x           |                   |                          |
-* 30 -| x             |                   |                          |
-*     |<  60 - 90 s  >|<    90 - 120 s   >|<       90 - 120 s       >|
-*     | Preheat Stage |   Soaking Stage   |       Reflow Stage       | Cool
-*  0  |_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
-*                                                                Time (Seconds)
+  based on ReflowOven
+    https://github.com/ohararp/ReflowOven
 
-Tamura RMA-20-21 Reflow Profile http://www.ese.com.sg/files/RMA-10-29N.pdf
-* 220-|                                                  x  x  
-*     |                                                x       x       
-* 200-|                                               x           x
-      |                                              x|           |
-*     |                                            x  |           | x   
-* 170-|                              x x x  x x  x    |           |   x 
-*     |                  x  x  x  x           |       |<- 20-60s->|     x
-* 150-|               x                       |                   |
-*     |             x |                       |                   |
-*     |           x   |                       |                   |
-*     |         x     |                       |                   |
-*     |       x       |                       |                   |
-*     |     x         |                       |                   |
-*     |   x   <3C/s   |        20C/60s        |         20/20s    |
-* 30 -| x             |                       |                   |
-*     |     <60s      |<-        60s        ->|                   |
-*     |     Preheat   |        Soaking        |       Reflow      |   Cool
-*  0  |_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _ _ _|_ _ _ _
-*                                                                 Time (Seconds)
+  - To start/abort reflow process push button.
+  - Buzzer beeps when cooling stage starts and ends. You may open door oven to cool quickly.
+  - LED and buzzer are optional but buzzer is recommended.
+  - Using with KOIZUMI Wide Oven Toaster 1200W KOS-1202
 **********************************************************************/
-
 // Includes
 #include <Adafruit_MAX31855.h>  // Thermocouple Library - https://github.com/rocketscream/MAX31855
 #include <PID_v1.h>             // PID Library - http://playground.arduino.cc/Code/PIDLibrary
@@ -72,24 +33,45 @@ typedef enum REFLOW_STATUS
   REFLOW_STATUS_ON
 } reflowStatus_t;
 
-// ***** CONSTANTS *****
+
+
+/*  Reflow temperature profile
+
+    Tamura leaded SOLDER PASTE(Sn62.8 Pb36.8 Ag0.4) Reflow Profile
+        |
+    220-|                                                  x  x
+        |                                                x       x
+    200-|                                               x           x
+        |                                              x|           |
+        |                                            x  |           | x
+    170-|                              x x x  x x  x    |           |   x
+        |                  x  x  x  x           |       |<- 20-60s->|     x
+    150-|               x                       |                   |
+        |             x |                       |                   |
+        |           x   |                       |                   |
+        |         x     |                       |                   |
+        |       x       |                       |                   |
+        |     x         |                       |                   |
+        |   x   <3C/s   |        20C/60s        |         20/20s    |
+    30 -| x             |                       |                   |
+        |     <60s      |<-        60s        ->|                   |
+        |     Preheat   |        Soaking        |       Reflow      |   Cool
+     0  |_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _ _ _|_ _ _ _
+*/
 float TEMPERATURE_COOL       = 100;
 float TEMPERATURE_SOAK_MIN   = 150;
 float TEMPERATURE_SOAK_MAX   = 170;
 float TEMPERATURE_REFLOW_MAX = 220;
-float TEMP_VALUE_MENU = 0;
 
-#define SENSOR_SAMPLING_TIME   1000
+// Preheat: 1.5C/s
+#define PREHEAT_TEMPERATURE_STEP    15
+#define PREHEAT_MS_STEP             10000
 
-// 2.5C/s
-#define PREHEAT_TEMPERATURE_STEP    10
-#define PREHEAT_MS_STEP             4000
-
-// 20C/60s
+// Soak: 20C/60s
 #define SOAK_TEMPERATURE_STEP       2
 #define SOAK_MS_STEP                6000
 
-// 20C/20s
+// Reflow: 20C/20s
 #define REFLOW_TEMPERATURE_STEP     5
 #define REFLOW_MS_STEP              5000
 
@@ -98,7 +80,7 @@ float TEMP_VALUE_MENU = 0;
 // ***** PRE-HEAT STAGE *****
 float PID_KP_PREHEAT         = 300;
 float PID_KI_PREHEAT         = 0.05;
-float PID_KD_PREHEAT         = 350; 
+float PID_KD_PREHEAT         = 350;
 // ***** SOAKING STAGE *****
 float PID_KP_SOAK            = 300;
 float PID_KI_SOAK            = 0.05;
@@ -106,10 +88,13 @@ float PID_KD_SOAK            = 350;
 // ***** REFLOW STAGE *****
 float PID_KP_REFLOW          = 300;
 float PID_KI_REFLOW          = 0.05;
-float PID_KD_REFLOW          = 350; 
-#define PID_SAMPLE_TIME 1000
+float PID_KD_REFLOW          = 350;
 
-// ***** LCD MESSAGES *****
+#define PID_SAMPLE_TIME 1000
+#define SENSOR_SAMPLING_TIME   1000
+
+
+// Reflow status strings
 const char* ssdMessagesReflowStatus[] = {
   "  Ready  ",
   " Preheat ",
@@ -121,15 +106,18 @@ const char* ssdMessagesReflowStatus[] = {
   "  Error  "
 };
 
+
+//
 // Pin Definitions
+//
 int HeaterPin = 5;  // Heater Element(active-high)
 int BuzPin = 6;     // Buzzer Pin
 int Led = 7;        // Heartbeat Led
 int But1 = 4;       // Start Button(pulluped, active-low)
-
 int thermocoupleCLKPin = A1;
 int thermocoupleSOPin  = A3;
 int thermocoupleCSPin  = A2;
+
 
 // ***** PID CONTROL VARIABLES *****
 double setpoint;
@@ -167,23 +155,23 @@ PID reflowOvenPID(&input, &output, &setpoint, kp, ki, kd, DIRECT);
 Adafruit_MAX31855 thermocouple(thermocoupleCLKPin, thermocoupleCSPin, thermocoupleSOPin);
 
 // Setup
-void setup()   {        
-  // Setup Serial Baudrate  
+void setup()   {
+  // Setup Serial Baudrate
   Serial.begin(9600);
 
   // initialize the Led pin as an output.
   pinMode(Led, OUTPUT);
- 
+
  // Init Buttons
-  pinMode(But1, INPUT_PULLUP); 
+  pinMode(But1, INPUT_PULLUP);
 
   // Init Elements
   pinMode(HeaterPin, OUTPUT);
   pinMode(BuzPin, OUTPUT);
-    
+
   digitalWrite(HeaterPin, LOW);
   digitalWrite(BuzPin, LOW);
-    
+
   // Set window size
   windowSize = 5000;
   // Initialize time keeping variable
@@ -192,10 +180,10 @@ void setup()   {
   nextRead = millis();
   // Set Relow Status = OFF
   reflowStatus = REFLOW_STATUS_OFF;
-  StartTest = false;  
+  StartTest = false;
 }
 
-// Begin Main Loop 
+// Begin Main Loop
 void loop()
 {
    // Current time
@@ -212,10 +200,10 @@ void loop()
           delay(10);
         }
       }
-      
+
       if (ButtCount>=5)
       {
-tone(BuzPin,4100,500); //Buzz the Buzzer  
+        tone(BuzPin,4100,500); //Buzz the Buzzer
         while (digitalRead(But1) != HIGH) ;
         reflowState = REFLOW_STATE_IDLE;
         if (reflowStatus == REFLOW_STATUS_ON) {
@@ -234,7 +222,7 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
   else
   {
     ButtCount = 0;
-    StartTest = false;    
+    StartTest = false;
   }
 
 //********************************************************************************************************
@@ -248,9 +236,9 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
     input = thermocouple.readCelsius();
     if(isnan(input))
     {
-      input=inputOld;  
+      input=inputOld;
       tcErrorCtr++;
-      
+
       if (tcErrorCtr >= 5)
       {
         // Illegal operation
@@ -260,10 +248,10 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
     }
     else
     {
-      tcErrorCtr = 0;  
+      tcErrorCtr = 0;
     }
   }
-  
+
   if (millis() > nextCheck)
   {
     // Check input in the next seconds
@@ -275,11 +263,11 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
       digitalWrite(Led, !(digitalRead(Led)));
       // Increase seconds timer for reflow curve analysis
       timerSeconds++;
-      // Send temperature and time stamp to serial 
+      // Send temperature and time stamp to serial
       Serial.print(timerSeconds);
       Serial.print(",");
-      Serial.print(thermocouple.readInternal());
-      Serial.print(",");
+      //Serial.print(thermocouple.readInternal());
+      //Serial.print(",");
       Serial.print(setpoint);
       Serial.print(",");
       Serial.print(input);
@@ -304,8 +292,8 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
       Serial.print("TC Error!\n");
     }
   }
-  
-  
+
+
   // Reflow oven controller state machine
   switch (reflowState)
   {
@@ -322,20 +310,21 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
         if (StartTest == true)
         {
           Serial.println(" - Begin  - ");
-       
-          // Send header for CSV file
           //Serial.println("Time Setpoint Input Output");
-          // Intialize seconds timer for serial debug information
           timerSeconds = 0;
+
           // Initialize PID control window starting time
           windowStartTime = millis();
-          // Ramp up to minimum soaking temperature
-          setpoint = TEMPERATURE_SOAK_MIN;
+
           // Tell the PID to range between 0 and the full window size
           reflowOvenPID.SetOutputLimits(0, windowSize);
           reflowOvenPID.SetSampleTime(PID_SAMPLE_TIME);
-          // Turn the PID on
           reflowOvenPID.SetMode(AUTOMATIC);
+
+          // Preheat profile
+          setpoint = input + PREHEAT_TEMPERATURE_STEP;
+          timerSoak = millis() + PREHEAT_MS_STEP;
+
           // Proceed to preheat stage
           reflowState = REFLOW_STATE_PREHEAT;
           StartTest = false;
@@ -345,22 +334,27 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
 
   case REFLOW_STATE_PREHEAT:
     reflowStatus = REFLOW_STATUS_ON;
-    // If minimum soak temperature is achieve       
-    if (input >= TEMPERATURE_SOAK_MIN-2.5)
+    if (millis() > timerSoak)
     {
-      // Chop soaking period into smaller sub-period
-      timerSoak = millis() + SOAK_MS_STEP;
-      // Set less agressive PID parameters for soaking ramp
-      reflowOvenPID.SetTunings(PID_KP_SOAK, PID_KI_SOAK, PID_KD_SOAK);
-      // Ramp up to first section of soaking temperature
-      setpoint = TEMPERATURE_SOAK_MIN + SOAK_TEMPERATURE_STEP;   
-      // Proceed to soaking state
-      reflowState = REFLOW_STATE_SOAK; 
+        setpoint = input + PREHEAT_TEMPERATURE_STEP;
+        if (setpoint > TEMPERATURE_SOAK_MIN) setpoint = TEMPERATURE_SOAK_MIN;
+        timerSoak = millis() + PREHEAT_MS_STEP;
+
+        // If minimum soak temperature is achieve
+        if (input >= TEMPERATURE_SOAK_MIN-2.5)
+        {
+          reflowOvenPID.SetTunings(PID_KP_SOAK, PID_KI_SOAK, PID_KD_SOAK);
+          setpoint = TEMPERATURE_SOAK_MIN + SOAK_TEMPERATURE_STEP;
+          timerSoak = millis() + SOAK_MS_STEP;
+
+          // Proceed to soaking state
+          reflowState = REFLOW_STATE_SOAK;
+        }
     }
   break;
 
-  case REFLOW_STATE_SOAK:     
-    // If micro soak temperature is achieved       
+  case REFLOW_STATE_SOAK:
+    // If micro soak temperature is achieved
     if (millis() > timerSoak)
     {
       timerSoak = millis() + SOAK_MS_STEP;
@@ -371,12 +365,12 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
         // Set agressive PID parameters for reflow ramp
         reflowOvenPID.SetTunings(PID_KP_REFLOW, PID_KI_REFLOW, PID_KD_REFLOW);
         // Ramp up to first section of soaking temperature
-        setpoint = TEMPERATURE_REFLOW_MAX;   
+        setpoint = TEMPERATURE_REFLOW_MAX;
         // Proceed to reflowing state
-        reflowState = REFLOW_STATE_REFLOW; 
+        reflowState = REFLOW_STATE_REFLOW;
       }
     }
-  break; 
+  break;
 
   case REFLOW_STATE_REFLOW:
     // We need to avoid hovering at peak temperature for too long
@@ -385,45 +379,45 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
     {
       // Set PID parameters for cooling ramp
       reflowOvenPID.SetTunings(PID_KP_REFLOW, PID_KI_REFLOW, PID_KD_REFLOW);
-      // Ramp down to minimum cooling temperature 
-      setpoint = TEMPERATURE_COOL;   
+      // Ramp down to minimum cooling temperature
+      setpoint = TEMPERATURE_COOL;
       // Proceed to cooling state
       reflowState = REFLOW_STATE_COOL;
-      
+
       // Turn Elements Off
       digitalWrite(HeaterPin, LOW);
-      
+
       // Signal to Open Door
       for (int i = 0; i<10;i++){
         tone(BuzPin,4100,100); //Buzz the Buzzer
         delay(250);
-      }       
-      
+      }
+
     }
-  break;   
+  break;
 
   case REFLOW_STATE_COOL:
-    // If minimum cool temperature is achieve       
+    // If minimum cool temperature is achieve
     if (input <= TEMPERATURE_COOL)
     {
       // Retrieve current time for buzzer usage
       buzzerPeriod = millis() + 1000;
-      
+
       for (int i = 0; i<10;i++){
-        tone(BuzPin,4100,500); //Buzz the Buzzer  
+        tone(BuzPin,4100,500); //Buzz the Buzzer
         delay(250);
       }
-      
-      reflowStatus = REFLOW_STATUS_OFF;                
+
+      reflowStatus = REFLOW_STATUS_OFF;
       // Proceed to reflow Completion state
-      reflowState = REFLOW_STATE_COMPLETE; 
-    }         
-  break;     
+      reflowState = REFLOW_STATE_COMPLETE;
+    }
+  break;
 
   case REFLOW_STATE_COMPLETE:
     if (millis() > buzzerPeriod)
     {
-      reflowState = REFLOW_STATE_IDLE; 
+      reflowState = REFLOW_STATE_IDLE;
     }
   break;
 
@@ -441,12 +435,12 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
     if(isnan(input))
     {
       // Wait until thermocouple wire is connected
-      reflowState = REFLOW_STATE_ERROR; 
+      reflowState = REFLOW_STATE_ERROR;
     }
     else
     {
       // Clear to perform reflow process
-      reflowState = REFLOW_STATE_IDLE; 
+      reflowState = REFLOW_STATE_IDLE;
     }
     break;
 
@@ -456,7 +450,7 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
       reflowStatus = REFLOW_STATUS_OFF;
       reflowState = REFLOW_STATE_IDLE;
     break;
-  }    
+  }
 
   // PID computation and SSR control
   if (reflowStatus == REFLOW_STATUS_ON)
@@ -466,24 +460,23 @@ tone(BuzPin,4100,500); //Buzz the Buzzer
     reflowOvenPID.Compute();
 
     if((now - windowStartTime) > windowSize)
-    { 
+    {
       // Time to shift the Relay Window
       windowStartTime += windowSize;
     }
-    if(output > (now - windowStartTime)) 
+    if(output > (now - windowStartTime))
     {
       digitalWrite(HeaterPin, HIGH);
     }
-    else 
+    else
     {
       digitalWrite(HeaterPin, LOW);
     }
   }
   // Reflow oven process is off, ensure oven is off
-  else 
+  else
   {
     digitalWrite(HeaterPin, LOW);
   }
-  
-}
 
+}
